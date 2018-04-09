59ae31786cd413cee4357d65f4e7695b950cc1cf
wyq <409232705@qq.com>
2018-04-09 22:34:04 +0800

    做了静态页面

---
+++ storage/framework/views/65b777a1802cd74c47a6403db41e37d28fa64a6f.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo $__env->yieldContent('title', 'Sample'); ?> - Laravel 新手入门教程</title>
+</head>
+<body>
+<?php echo $__env->yieldContent('content'); ?>
+</body>
+</html>
